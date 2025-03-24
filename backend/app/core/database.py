8363421bf1ad4a8c46799e0cf29c6bf2f51d4753
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-03-25 01:38:18 +0500

    a bd

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 from app.core.base import Base  
-
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
